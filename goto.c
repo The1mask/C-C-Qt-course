506ff4f526395b73ede8c809b1008cdc6ebbdd2d
the1mask <the1mask112@gmail.com>
2019-04-26 14:59:52 -0400

    Vrode rabotaet

--- goto.c
+++ goto.c
@@ -10,13 +10,21 @@ else {
 	char a[10000];
 	int b=0;
 	for(int i=0;i<9999;i++){
+		
 		a[i]=fgetc(fp);
+		if(!feof(fp)){
 		//printf("%c", a[i]);
 		for(int m=0;m<9999;m++){
 			b=b+sizeof(a[i]);
+		}
+		}
+		else{
+			a[i]='a';
+			
+			
 			
 		}}
-		printf("\n %d %c %c %c", b, a[999], a[998], a[997]);
+		printf("\n %d  5: %c 6 : %c 7 : %c 8: %c 9: %c", b, a[9995], a[9996], a[9997], a[9998], a[9999]);
 	return 1;
 	}}
 			
