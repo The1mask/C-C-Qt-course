5b2928ae64ae304fbb2ca0beb490fd82733ba157
the1mask <the1mask112@gmail.com>
2019-05-24 14:22:12 -0400

    rabotaet

--- 9dz/sortirovka.c
+++ 9dz/sortirovka.c
@@ -18,56 +18,73 @@ void bubble(int *num){
 	}
 }
 
+void print(float *num1, float *num2){
+	
+	printf("\n %f", num2[1]);
+	
+	qsort(num1, 10, sizeof(int), sort);
+	qsort(num2, 10, sizeof(int), sort);
+}
+
 int main(){
 	int arr[1000];
+	float a[10];
+	float b[10];
+	float c[2]={0,0};
+	
+	
+	
+	clock_t time;
+	
+	for(int i=0;i<10;i++){
 	
 	for(int a=0;a<1000;a++)
 		arr[a]=rand()%19;
 	
-	clock_t time;
 	time=clock();
 	
 	qsort(arr, 1000, sizeof(int), sort);
-	for(int i=0;i<1000;i++){
-		printf("%d", arr[i]);
-	}
+		
+	a[i]=clock() - time;
 	
-	time=clock() - time;
-	printf("%f", (double)time/CLOCKS_PER_SEC);
+	 
+	 
+	printf("\n qsort:a %f", ((float)a[i]/CLOCKS_PER_SEC));
 	
+		}
+		
+	for(int i=0;i<10;i++){
 	
-	printf("\n");
+	for(int a=0;a<1000;a++)
+		arr[a]=rand()%19;
+	
 	
 	time=clock();
 	
 	bubble(arr);
 	
-	for(int i=0;i<1000;i++){
-	printf("%d", arr[i]);
-}	
+	b[i]=clock() - time;
+	printf("\n bubble: %f", ((float)b[i]/CLOCKS_PER_SEC));
+	}
+	
+	print(b, a);
+	for(int i=0;i<10;i++){
+		c[0]=c[0]+a[i];
+		c[1]=c[1]+b[i];
+		
+		
+	
+	}
+	c[0]=c[0]/10000000;
+	c[1]=c[1]/10000000;
 	
 	
-	
-	
-	
-	
-	
-	
-	time=clock() - time;
-	printf("%f", (double)time/CLOCKS_PER_SEC);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		printf("\n bubble minimum: %f", ((float)b[0]/CLOCKS_PER_SEC));
+		printf("\n qsort minimum: %f", ((float)a[0]/CLOCKS_PER_SEC));
+		printf("\n bubble maximum: %f", ((float)b[9]/CLOCKS_PER_SEC));
+		printf("\n qsort maximum: %f", ((float)a[9]/CLOCKS_PER_SEC));
+		printf("\n bubble sred: %f", c[1]);
+		printf("\n qsort sred: %f", c[0]);
+		
 	return 0;
 }
